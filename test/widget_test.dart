25e8fa4c7c86a438d99b906728c8c92011527d5f
Emanuelle Passos <62838314+Emanuelle-p@users.noreply.github.com>
2023-08-23 22:02:04 -0300

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:visitantes_condominio/main.dart';
+import 'package:veiculos/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
